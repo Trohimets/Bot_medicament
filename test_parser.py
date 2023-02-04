c875b5463bca16d932d864adcf6c2f87d1d5da52
Константин Трохимец <katrohimets@mail.ru>
2023-02-04 23:16:32 +0300

    add functions for bot

--- test_parser.py
+++ test_parser.py
@@ -1,10 +1,28 @@
 import requests
-from bs4 import BeautifulSoup
 
 
 search_name = 'Квамател'
-url = f'https://jnvlpcalc.spbeias.ru/api/JNVLP/GetFilteredData?chunkName={search_name}&chunkPackage=&chunkProducer='
-r = requests.get(url)
-data = r.json()
-for element in data:
-    print(element['producer'], element['package'])
+package = 'таблетки, покрытые пленочной оболочкой, 20 мг, 14 шт. - блистер (2)  - пачка картонная'
+
+def get_json(search_name):
+    url = f'https://jnvlpcalc.spbeias.ru/api/JNVLP/GetFilteredData?chunkName={search_name}&chunkPackage=&chunkProducer='
+    r = requests.get(url)
+    data = r.json()
+    return data
+
+def get_package(data):
+    result = []
+    for element in data:
+        result.append(element['package'])
+    return result
+
+def get_producer(data, package):
+    result = []
+    i = 1
+    for element in data:
+        if element['package'] == package:
+            result.append(element['producer'])
+    print(len(result))
+
+data = get_json(search_name)
+get_producer(data, package)
