c8999597abfebb7d974f064f96f2477829f51efb
Константин Трохимец <katrohimets@mail.ru>
2023-02-19 21:03:18 +0300

    изменил логику функций, чтобы сначала выбирать производителя

--- price_parser.py
+++ price_parser.py
@@ -3,6 +3,7 @@ import requests
 
 search_name = 'Ацетилсалициловая кислота'
 package = 'таблетки, покрытые пленочной оболочкой, 20 мг, 14 шт. - блистер (2)  - пачка картонная'
+producer = 'Вл.Общество с ограниченной ответственностью "ПФКО-1", Россия (5404070404); Вып.к.Перв.Уп.Втор.Уп.Пр.Акционерное общество "Производственная фармацевтическая компания Обновление" (АО "ПФК Обновление"), Россия (5408151534); '
 
 def get_json(search_name):
     url = f'https://jnvlpcalc.spbeias.ru/api/JNVLP/GetFilteredData?chunkName={search_name}&chunkPackage=&chunkProducer='
@@ -10,19 +11,20 @@ def get_json(search_name):
     data = r.json()
     return data
 
-def get_package(data):
+def get_package(data, producer):
     result = []
     for element in data:
-        result.append(element['package'])
+        if element['producer'] == producer:
+            result.append(element['package'])
     return result
 
-def get_producer(data, package):
+def get_producer(data):
     result = []
-    i = 1
     for element in data:
-        if element['package'] == package:
-            result.append(element['producer'])
+        result.append(element['producer'])
     return result
 
+# print(get_json(search_name))
 data = get_json(search_name)
-get_producer(data, package)
+# print(get_producer(data))
+print(get_package(data, producer))
